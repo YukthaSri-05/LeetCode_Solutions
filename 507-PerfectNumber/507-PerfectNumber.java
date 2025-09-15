a3f689c31e396fa7ca71226964b20275573d4d41
Yuktha sri Nunna <23mh1a05n8@acoe.edu.in>
2025-09-15 20:39:20 +0530

    Create 507-PerfectNumber

---
+++ 507-PerfectNumber/507-PerfectNumber.java
@@ -0,0 +1,16 @@
+// Last updated: 9/15/2025, 8:39:20 PM
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        if(num<=1) return false;
+        int sum=1;
+        for(int i=2;i*i<=num;i++){
+            if(num%i==0){
+                sum+=i;
+                if(i!=num/i){
+                    sum+=num/i;
+                }
+            }
+        }
+        return sum == num;
+    }
+}
