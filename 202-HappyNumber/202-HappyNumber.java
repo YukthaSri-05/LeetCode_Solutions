01cd247b447032d5e79696bd26e6dbbe8c4f985f
Yuktha sri Nunna <23mh1a05n8@acoe.edu.in>
2025-09-15 20:39:54 +0530

    Create 202-HappyNumber

---
+++ 202-HappyNumber/202-HappyNumber.java
@@ -0,0 +1,26 @@
+// Last updated: 9/15/2025, 8:39:54 PM
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public boolean isHappy(int n) {
+        Set<Integer> seen = new HashSet<>();
+
+        while (n != 1 && !seen.contains(n)) {
+            seen.add(n);
+            n = getSquareSum(n);
+        }
+
+        return n == 1;
+    }
+
+    private int getSquareSum(int n) {
+        int sum = 0;
+        while (n > 0) {
+            int digit = n % 10;
+            sum += digit * digit;
+            n /= 10;
+        }
+        return sum;
+    }
+}
