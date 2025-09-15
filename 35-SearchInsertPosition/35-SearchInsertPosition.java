3346968af5fd6919751c2afb0d3a86e2eccc66b1
Yuktha sri Nunna <23mh1a05n8@acoe.edu.in>
2025-09-15 20:40:19 +0530

    Create 35-SearchInsertPosition

---
+++ 35-SearchInsertPosition/35-SearchInsertPosition.java
@@ -0,0 +1,17 @@
+// Last updated: 9/15/2025, 8:40:19 PM
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2; 
+            if (nums[mid] == target) {
+                return mid; 
+            } else if (nums[mid] < target) {
+                left = mid + 1; 
+            } else {
+                right = mid - 1; 
+            }
+        }
+        return left;
+    }
+}
