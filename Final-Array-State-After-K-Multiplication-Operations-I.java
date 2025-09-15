ba3823be9c6389b71db38d86e815720184a1e319
Yuktha sri Nunna <23mh1a05n8@acoe.edu.in>
2025-09-15 12:08:42 +0530

    [3264] [Time Beats: 100.00%] [Memory Beats: 78.66%] - LeetPush

---
+++ Final-Array-State-After-K-Multiplication-Operations-I.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] getFinalState(int[] nums, int k, int multiplier) {
+        int n = nums.length;
+        for (int i = 0; i < k; i++) {
+            int minIndex = 0;
+            for (int j = 1; j < n; j++) {
+                if (nums[j] < nums[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            nums[minIndex] *= multiplier;
+        }
+        return nums;
+    }
+}
