01a4600112e0fbc038d021714a20171bfd552468
Yuktha sri Nunna <23mh1a05n8@acoe.edu.in>
2025-09-15 20:38:09 +0530

    Create 3194-FindWordsContainingCharacter

---
+++ 3194-FindWordsContainingCharacter/3194-FindWordsContainingCharacter.java
@@ -0,0 +1,13 @@
+// Last updated: 9/15/2025, 8:38:09 PM
+import java.util.*;
+class Solution {
+    public List<Integer> findWordsContaining(String[] words, char x) {
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].indexOf(x) != -1) {  
+                result.add(i);
+            }
+        }
+        return result;
+    }
+}
