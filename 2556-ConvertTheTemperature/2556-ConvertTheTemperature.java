39f3213638c64b44c4441e634166618b0613e633
Yuktha sri Nunna <23mh1a05n8@acoe.edu.in>
2025-09-15 20:38:14 +0530

    Create 2556-ConvertTheTemperature

---
+++ 2556-ConvertTheTemperature/2556-ConvertTheTemperature.java
@@ -0,0 +1,8 @@
+// Last updated: 9/15/2025, 8:38:14 PM
+class Solution {
+    public double[] convertTemperature(double celsius) {
+        double kelvin = celsius + 273.15;
+        double fahrenheit = celsius * 1.80 + 32.00;
+        return new double[]{kelvin, fahrenheit};
+    }
+}
