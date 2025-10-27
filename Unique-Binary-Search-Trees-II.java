2677795337fa4dcd912e549f5712c6fcbb232c47
Yuktha sri Nunna <23mh1a05n8@acoe.edu.in>
2025-10-27 11:28:45 +0530

    [95] [Time Beats: 99.08%] [Memory Beats: 15.45%] - LeetPush

---
+++ Unique-Binary-Search-Trees-II.java
@@ -0,0 +1,61 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    public List<TreeNode> generateTrees(int n) {
+        if (n == 0) return new ArrayList<>();
+        return build(1, n);
+    }
+
+    private List<TreeNode> build(int start, int end) {
+        List<TreeNode> allTrees = new ArrayList<>();
+        if (start > end) {
+            allTrees.add(null);
+            return allTrees;
+        }
+
+        // Choose i as root
+        for (int i = start; i <= end; i++) {
+            // Generate all possible left and right subtrees
+            List<TreeNode> leftTrees = build(start, i - 1);
+            List<TreeNode> rightTrees = build(i + 1, end);
+
+            // Combine each left and right with current root i
+            for (TreeNode left : leftTrees) {
+                for (TreeNode right : rightTrees) {
+                    TreeNode root = new TreeNode(i);
+                    root.left = left;
+                    root.right = right;
+                    allTrees.add(root);
+                }
+            }
+        }
+
+        return allTrees;
+    }
+}
